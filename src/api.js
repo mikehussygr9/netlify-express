2afa50957204a763b6d268925f55b85b48b044be
Anup Kumar Sah <anupkumarsah@Anups-MacBook-Pro.local>
2021-10-27 01:01:58 +0530

    first commit8

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const requestIp = require('request-ip');
+
+const router=express.Router();
+
+const app = express();
+router.get('/',(req, res) =>{
+
+    const clientIp = requestIp.getClientIp(req);
+    console.log(clientIp);
+    //res.json({clientIp});
+    res.send("clientIp");
+
+});
+app.use('/.netlify/functions/api',router);
+module.exports.handler=serverless(app);
