42b040da7a5b79f36ee30de157a55d42a0178f7d
cifergciferg@gmail.com <cifergciferg@gmail.com>
2021-10-27 12:16:26 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const requestIp = require('request-ip');
+
+const router=express.Router();
+
+const app = express();
+router.get('/',(req, res) =>{
+
+    const clientIp = requestIp.getClientIp(req);
+    //const geo = geoip.lookup(ipg);
+    const geo = geoip.lookup(clientIp.toString());
+    if (geo.country==="IN")
+    {
+        response.redirect('https://www.facebook.com/goo');
+    }
+    else
+    {
+        response.redirect("https://www.google.com/");
+    }
+    console.log(clientIp);
+    //res.json({clientIp});
+    res.send(clientIp);
+
+});
+app.use('/.netlify/functions/api',router);
+module.exports.handler=serverless(app);
